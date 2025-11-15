293fccbae9a9375b9357006d1640f098899573ae
Silas Henrique <silash35@gmail.com>
2025-11-15 20:41:07 -0300

    Give up simulating

---
+++ src/lib/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams.update(
+    {
+        "figure.figsize": (16, 6),
+        # 'figure.dpi': 300,
+        "font.size": 15,
+        "legend.loc": "upper right",
+    }
+)
