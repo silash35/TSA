523194ecbe46bd597fbe215a5365ca4052fcb6ed
Silas Henrique <silash35@gmail.com>
2025-11-20 21:46:01 -0300

    Improve plots

--- src/lib/plot.py
+++ src/lib/plot.py
@@ -6,37 +6,53 @@ plt.rcParams.update(
         "figure.autolayout": True,
         "axes.grid": True,
         # 'figure.dpi': 300,
-        "font.size": 15,
-        "legend.loc": "upper right",
+        "font.size": 16,
     }
 )
 
 
+def legend_outside(ax: plt.Axes):
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5), frameon=False)
+
+
 def plot_comparison(cycles, real_y, model_y, model_name):
-    plt.figure()
+    _, axes = plt.subplots(2, 1, sharex=True)
+    variables = [
+        ("Purity H2", "tab:blue", 0),
+        ("H2/CO ratio", "tab:orange", 1),
+        ("Purity CO2", "tab:green", 2),
+        ("Recovery CO2", "tab:red", 3),
+        ("Productivity", "tab:purple", 4),
+    ]
 
-    plt.scatter(cycles, real_y[:, 0], label="Purity H2")
-    plt.plot(cycles, model_y[:, 0], label=f"Purity H2 ({model_name})")
+    group1 = [0, 2, 3]
+    ymin = min(real_y[:, variables[i][2]].min() for i in group1)
+    ymax = max(real_y[:, variables[i][2]].max() for i in group1)
+    ax = axes[0]
+    for i in group1:
+        label, color, idx = variables[i]
+        ax.scatter(cycles, real_y[:, idx], label=f"{label} (gPROMS)", color=color)
+        ax.plot(cycles, model_y[:, idx], label=f"{label} ({model_name})", color=color)
+    ax.set_ylim(ymin * 0.9, ymax * 1.1)
+    ax.set_ylabel("Valores")
+    legend_outside(ax)
 
-    plt.scatter(cycles, real_y[:, 1], label="H2/CO ratio")
-    plt.plot(cycles, model_y[:, 1], label=f"H2/CO ratio ({model_name})")
+    group2 = [1, 4]
+    ymin = min(real_y[:, variables[i][2]].min() for i in group2)
+    ymax = max(real_y[:, variables[i][2]].max() for i in group2)
+    ax = axes[1]
+    for i in group2:
+        ax = axes[1]
+        label, color, idx = variables[i]
+        ax.scatter(cycles, real_y[:, idx], label=f"{label} (gPROMS)", color=color)
+        ax.plot(cycles, model_y[:, idx], label=f"{label} ({model_name})", color=color)
+    ax.set_ylim(ymin * 0.9, ymax * 1.1)
+    ax.set_ylabel("Valores")
+    legend_outside(ax)
 
-    plt.scatter(cycles, real_y[:, 2], label="Purity CO2")
-    plt.plot(cycles, model_y[:, 2], label=f"Purity CO2 ({model_name})")
+    axes[-1].set_xlabel("Ciclo")
 
-    plt.scatter(cycles, real_y[:, 3], label="Recovery CO2")
-    plt.plot(cycles, model_y[:, 3], label=f"Recovery CO2 ({model_name})")
-
-    plt.scatter(cycles, real_y[:, 4], label="Productivity")
-    plt.plot(cycles, model_y[:, 4], label=f"Productivity ({model_name})")
-
-    plt.ylim(0, real_y.max() * 1.1)
-
-    plt.xlabel("Ciclo")
-    plt.ylabel("Saída")
-    plt.legend()
     # plt.show()
-
     plt.savefig(f"../figures/comparision-{model_name}.png")
     plt.close()
 
